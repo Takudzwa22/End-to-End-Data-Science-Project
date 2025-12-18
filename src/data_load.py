d8026ce35a5e4adb6b288fc9bd34f3bf276e1619
Taku Mambwere <132899881+Takudzwa22@users.noreply.github.com>
2025-12-18 14:33:25 +0100

    Remove from load_raw function
    Removed  from load_raw function.

--- src/data_load.py
+++ src/data_load.py
@@ -2,9 +2,6 @@ import pandas as pd
 from .config import CONFIG
 
 def load_raw() -> pd.DataFrame:
-    """Load raw dataset from CONFIG.DATA_PATH.
-
-    You must download the dataset and place the CSV at the configured path.
-    """
+    
     df = pd.read_csv(CONFIG.DATA_PATH)
     return df
